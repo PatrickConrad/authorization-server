ad4acf739d7127c09107ce651378106550d4b454
MarylandGrown <paleagles93@yahoo.com>
2021-05-21 21:37:27 -0400

    10 commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -7,9 +7,9 @@ authRouter.post('/login', middleware.auth.isLoggedIn, controllers.auth.login);
 authRouter.post('/register', middleware.auth.isLoggedIn, controllers.auth.register);
 authRouter.put('/forgot-password', middleware.auth.isLoggedIn, controllers.auth.forgotPassword);
 authRouter.put('/reset-password/:token', controllers.auth.resetPassword);
-authRouter.put('/update-access', controllers.auth.updateAccess);
-authRouter.post('/update-refresh', controllers.auth.updateRefresh);
-authRouter.delete('/logout', controllers.auth.logout);
+authRouter.put('/update-access', middleware.auth.hasRefresh, controllers.auth.updateAccess);
+authRouter.post('/update-refresh',  middleware.auth.hasRefresh, controllers.auth.updateRefresh);
+authRouter.delete('/logout',  middleware.auth.hasAccess, controllers.auth.logout);
 
 
 module.exports = authRouter;
